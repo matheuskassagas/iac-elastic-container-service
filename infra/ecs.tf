d5ceb56212639bd937434ea1564709a9960c5fe6
matheuskassagas <gigliotti_@hotmail.com>
2022-08-09 14:57:18 -0300

    ECS-com-image-docker

---
+++ infra/ecs.tf
@@ -0,0 +1,43 @@
+#######
+
+# # Cluster modulo aws
+
+module "ecs" {
+  source = "terraform-aws-modules/ecs/aws"
+
+  name               = var.ambiante
+  container_insights = true
+  capacity_providers = ["FARGATE"]
+
+  default_capacity_provider_strategy = [
+    {
+      capacity_provider = "FARGATE"
+    }
+  ]
+}
+
+resource "aws_ecs_task_definition" "Django-API" {
+  family                   = "Django-API"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.cargo.arn
+  container_definitions = jsonencode (
+    [
+      {
+        "name"      = "producao"
+        "image"     = "<name from image created>"
+        "cpu"       = 256
+        "memory"    = 512
+        "essential" = true
+        "portMappings" = [
+          {
+            "containerPort" = 8000
+            "hostPort"      = 8000
+          }
+        ]
+      }
+    ]
+  )
+}
