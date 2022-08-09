f88c4055f4aae9b4c4bbf0fd1ee7447469a46cd6
matheuskassagas <gigliotti_@hotmail.com>
2022-08-09 15:11:20 -0300

    ecs service

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -41,3 +41,26 @@ resource "aws_ecs_task_definition" "Django-API" {
     ]
   )
 }
+
+resource "aws_ecs_service" "Django-API" {
+  name            = "Django-API"
+  cluster         = module.ecs.ecs_cluster_id
+  task_definition = aws_ecs_task_definition.Django-API.arn
+  desired_count   = 3
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.alvo.arn
+    container_name   = "producao"
+    container_port   = 8000
+  }
+
+  network_configuration {
+      subnets = module.vpc.private_subnets
+      security_groups = [aws_security_group.privado.id]
+  }
+
+  capacity_provider_strategy {
+      capacity_provider = "FARGATE"
+      weight = 1 #100/100
+  }
+}
