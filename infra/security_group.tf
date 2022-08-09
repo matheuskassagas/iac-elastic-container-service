c10ef0b173fc8f53e6422e73fd18b869173369e2
matheuskassagas <gigliotti_@hotmail.com>
2022-08-09 11:20:42 -0300

    grupo de seguranca

--- infra/security_group.tf
+++ infra/security_group.tf
@@ -3,7 +3,7 @@ resource "aws_security_group" "alb" {
   vpc_id = module.vpc.vpc_id
 }
 
-resource "aws_secutiry_group_rule" "alb_role"{
+resource "aws_secutiry_group_rule" "alb_role_entrada"{
   type = "ingress"
   from_port = 8000
   to_port = 8000
@@ -12,7 +12,7 @@ resource "aws_secutiry_group_rule" "alb_role"{
   security_group_id = aws_security_group.alb.id
 }
 
-resource "aws_secutiry_group_rule" "alb_role"{
+resource "aws_secutiry_group_rule" "alb_role_saida" {
   type = "egress"
   from_port = 0
   to_port = 0
@@ -21,3 +21,25 @@ resource "aws_secutiry_group_rule" "alb_role"{
   security_group_id = aws_security_group.alb.id
 }
 
+resource "aws_security_group" "private" {
+  name = "private_ECS"
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_secutiry_group_rule" "entrada_ecs"{
+  type = "ingress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  source_security_group_id = aws_security_group.alb.id # # JUST FROM ALB
+  security_group_id = aws_security_group.privado.id
+}
+
+resource "aws_secutiry_group_rule" "saida_ecs"{
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"] # # 0.0.0.0 -> 255.255.255.255
+  security_group_id = aws_security_group.privado.id
+}
